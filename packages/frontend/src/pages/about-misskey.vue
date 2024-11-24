40b6902bef714e056c0b73c93e389072e3eb2130
m-hayabusa <s.hashimoto@hs-sh.net>
2024-11-24 05:18:18 +0000

    リンク先を戻す

--- packages/frontend/src/pages/about-misskey.vue
+++ packages/frontend/src/pages/about-misskey.vue
@@ -29,7 +29,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 				</div>
 				<FormSection>
 					<div class="_gaps_s">
-						<FormLink to="https://github.com/m-hayabusa/misskey/tree/mewlme_v13" external>
+						<FormLink to="https://github.com/misskey/misskey" external>
 							<template #icon><i class="ti ti-code"></i></template>
 							{{ i18n.ts._aboutMisskey.source }} ({{ i18n.ts._aboutMisskey.original }})
 							<template #suffix>GitHub</template>
